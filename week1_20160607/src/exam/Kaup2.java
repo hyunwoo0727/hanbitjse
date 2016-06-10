2b6b908e7e825abe4bcc95aa96264146b054be8d
USER <USER@hb1030>
2016-06-10 11:36:49 +0900

    kaup2

---
+++ week1_20160607/src/exam/Kaup2.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package exam;
+
+import java.util.Scanner;
+
+/**
+ * @date   : 2016. 6. 10.
+ * @author : HyunWoo Lee
+ * @file   : Kaup2.java 
+ * @story  : pattern 
+*/
+public class Kaup2 {
+	
+	public static void main(String[] args) {
+		//-----------initializing------------------//
+		Scanner scan = new Scanner(System.in);
+		//-----------operation------------------//
+		
+		//-----------console----------------//
+		System.out.println("");
+		
+	}
+	
+}
