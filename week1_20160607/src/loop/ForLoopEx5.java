c717b2f8d4202fa3685f4298f9fccc86f571f209
USER <USER@hb1030>
2016-06-13 15:55:24 +0900

    loop

--- week1_20160607/src/loop/ForLoopEx5.java
+++ week1_20160607/src/loop/ForLoopEx5.java
@@ -14,7 +14,6 @@ import java.util.Scanner;
 public class ForLoopEx5 {
 	public static void main(String[] args) {
 		//-----------initializing------------------//
-		Scanner scan = new Scanner(System.in);
 		//-----------operation------------------//
 		for (int i = 1 ; i <= 10 ; i++) {
 			if(i%3==0){
