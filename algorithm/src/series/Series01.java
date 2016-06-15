f333858528e70c72cc4f3862061d442016cf98c2
USER <USER@hb1030>
2016-06-15 18:32:31 +0900

    dddddddd

--- algorithm/src/series/Series01.java
+++ algorithm/src/series/Series01.java
@@ -17,22 +17,10 @@ public class Series01 {
 	 * */
 	
 	public static void main(String[] args) {
-		/**
-		 * 나열    i     ???
-		 * 1 =  0 + ?    ?
-		 * 2 =  1 + ?    1 
-		 * 4 =  2 + ?    2
-		 * 7 =  3 + ?    4
-		 * 11 = 4 + ?    7
-		 * 16 = 5 + ?   11 
-		 20항 20번 
-		 합계...공차
-		 */
-		
+				
 		int i=0,diff=1,sum=0;
-		// 0~19 i
-		// diff 1 2 3 4 5 6 7 8 9 10 ..
-		// 숫자 1 2 4 7 11 16 22 
+		// i 1 2 3 4 5 6 7 8 9 10 ..
+		// diff 1 2 4 7 11 16 22 
 		for (; i < 20; i++){
 			diff += i;
 			System.out.printf("%d ",diff);
