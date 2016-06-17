c7a06cb5fc51b6841f115cf968db90c6677db39e
USER <USER@hb1030>
2016-06-17 09:24:07 +0900

    something

---
+++ oop/src/something/Something.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package something;
+
+/**
+ * @date : 2016. 6. 17.
+ * @author : HyunWoo Lee
+ * @file : Something.java
+ * @story :
+ */
+public class Something {
+	private String aaa;
+	private String bbb;
+	private int ccc;
+	private int ddd;
+	
+	public Something(String aaa,String bbb,int ccc,int ddd){
+		
+		this.aaa = aaa;
+		this.bbb = bbb;
+		this.ccc = ccc;
+		this.ddd = ddd;
+	}
+	
+	public String getAaa() {
+		return aaa;
+	}
+	public String getBbb() {
+		return bbb;
+	}
+	public void setBbb(String bbb) {
+		this.bbb = bbb;
+	}
+	public int getCcc() {
+		return ccc;
+	}
+	public int getDdd() {
+		return ddd;
+	}
+	
+	
+	
+}
---
+++ oop/src/something/SomethingController.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package something;
+
+import javax.swing.JOptionPane;
+
+import jdk.nashorn.internal.scripts.JO;
+
+/**
+ * @date   : 2016. 6. 17.
+ * @author : HyunWoo Lee
+ * @file   : SomethingController.java 
+ * @story  : 
+*/
+public class SomethingController {
+	public static void main(String[] args) {
+		Something smt= null;
+		
+		while (true) {
+			switch (JOptionPane.showInputDialog("1.등록 2.조회 0.종료")) {
+			case "1":
+				String aaa = JOptionPane.showInputDialog("aaa(문자)");
+				String bbb = JOptionPane.showInputDialog("bbb(문자)");
+				String ccc = JOptionPane.showInputDialog("ccc(숫자)");
+				int ic = Integer.parseInt(ccc);
+				
+				String ddd = JOptionPane.showInputDialog("ddd(숫자)");
+				int d = Integer.parseInt(ddd);
+				smt = new Something(aaa, bbb,ic ,d);
+				break;
+			case "2":
+				if(smt==null){
+					JOptionPane.showMessageDialog(null, "등록 먼저 해주세요");
+					break;
+				}
+				JOptionPane.showMessageDialog(null, "aaa: " + smt.getAaa() + "\nbbb: " + smt.getBbb() +
+						"\nccc: " + smt.getCcc() + "\nddd: " + smt.getDdd());
+				break;
+			case "0":
+				if(JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?")==0){
+					JOptionPane.showMessageDialog(null, "SYSTEM OFF");
+					return;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
