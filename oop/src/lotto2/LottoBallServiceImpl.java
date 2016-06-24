b298afeacc524a92765ec20a078aa2beba0b5650
USER <USER@hb1030>
2016-06-24 13:55:32 +0900

    로오또볼

--- oop/src/lotto2/LottoBallServiceImpl.java
+++ oop/src/lotto2/LottoBallServiceImpl.java
@@ -12,19 +12,23 @@ public class LottoBallServiceImpl implements LottoBallService {
 		winLotto = new int[6];
 		lottoBean = new LottoBean();
 		int index=0;
+		boolean flag = false;
 		while(index<6){
+			flag=false;
 			lottoBean.setNumber();
 			for (int j=0; j < winLotto.length; j++) {
 				if(winLotto[j]==lottoBean.getNumber()){
-					index--;
-					continue;
+					flag=true;
+					break;
 				}
 			}
-			winLotto[index] = lottoBean.getNumber();
-			index++;
+			if(!flag){
+				winLotto[index] = lottoBean.getNumber();
+				index++;
+			}
 		}
 		Arrays.sort(winLotto);
-}
+	}
 	@Override
 	public int[] getLottoball() {
 		// TODO Auto-generated method stub
