6f1821cf70011c8456d190b09c6c356f15601a10
hb2011 <hb2011@lee-PC>
2016-06-28 16:46:27 +0900

    uuu

--- oop/src/map/MemberServiceImpl.java
+++ oop/src/map/MemberServiceImpl.java
@@ -12,84 +12,83 @@ import java.util.Map;
 */
 
 public class MemberServiceImpl implements MemberService{
-	private Map<String, MemberBean> list;
+	private Map<String, MemberBean> map;
 	
 	public MemberServiceImpl() {
-		list = new HashMap<String, MemberBean>();
+		map = new HashMap<String, MemberBean>();
 	}
 	@Override
 	public String join(MemberBean mBean) {
-		if(list.containsKey(mBean.getId())){
+		if(map.containsKey(mBean.getId())){
 			return "아이디가 이미 있습니다";
 		}
-		this.list.put(mBean.getId(), mBean);
+		this.map.put(mBean.getId(), mBean);
 		return mBean.getId()+" 님 가입완료!";
 	}
 	@Override
 	public String login(MemberBean mBean) {
-		// TODO Auto-generated method stub
-		if(list.containsKey(mBean.getId())){
-			return this.list.get(mBean.getId()).getPw().equals(mBean.getPw()) ? mBean.getId() + "님 환영합니다" : "로그인 실패";
+		if(map.containsKey(mBean.getId())){
+			return this.map.get(mBean.getId()).getPw().equals(mBean.getPw()) ? mBean.getId() + "님 환영합니다" : "로그인 실패";
 		}
 		return "아이디가 없습니다";
 	}
 	@Override
 	public MemberBean detail(String id) {
 		// TODO Auto-generated method stub
-		return this.list.get(id);
+		return this.map.get(id);
 	}
 	@Override
 	public Map<String, MemberBean> list() {
-		return this.list;
+		return this.map;
 	}
 	@Override
 	public MemberBean findById(String id) {
-		return list.containsKey(id) ? list.get(id) : null;
+		return map.containsKey(id) ? map.get(id) : null;
 	}
 	@Override
 	public Map<String, MemberBean> findByName(String name) {
-		Map<String, MemberBean> tempList = new HashMap<String,MemberBean>();
+		Map<String, MemberBean> tempMap = new HashMap<String,MemberBean>();
 		MemberBean tempBean = null;
-		Iterator<MemberBean> it = list.values().iterator();
+		Iterator<MemberBean> it = map.values().iterator();
 		while(it.hasNext()){
 			tempBean = it.next();
 			if(tempBean.getName().equals(name)){
-				tempList.put(tempBean.getId(), tempBean);
+				tempMap.put(tempBean.getId(), tempBean);
 			}
 		}
-		return tempList;
+		return tempMap;
 	}
 	@Override
 	public String updatePw(MemberBean mBean) {
-		if(list.containsKey(mBean.getId())){
-			list.get(mBean.getId()).setPw(mBean.getPw());           
+		if(map.containsKey(mBean.getId())){
+			map.get(mBean.getId()).setPw(mBean.getPw());           
 			return "변경완료";
 		}
 		return "아이디가 없습니다";
 	}
 	@Override
 	public String delete(String id) {
-		if(list.containsKey(id)){
-			list.remove(id);
+		if(map.containsKey(id)){
+			map.remove(id);
 			return "탈퇴완료";
 		}
 		return "아이디가 없습니다";
 	}
 	@Override
 	public int count() {
-		return this.list.size();
+		return this.map.size();
 	}
 	@Override
 	public Map<String, MemberBean> findByGender(String gender) {
-		Map<String, MemberBean> tempList = new HashMap<String,MemberBean>();
+		Map<String, MemberBean> tempMap = new HashMap<String,MemberBean>();
 		MemberBean tempBean = null;
-		Iterator<MemberBean> it = list.values().iterator();
+		Iterator<MemberBean> it = map.values().iterator();
 		while(it.hasNext()){
 			tempBean = it.next();
 			if(tempBean.getGender().equals(gender)){
-				tempList.put(tempBean.getId(), tempBean);
+				tempMap.put(tempBean.getId(), tempBean);
 			}
 		}
-		return tempList;
+		return tempMap;
 	}
 }
